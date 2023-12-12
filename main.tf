71ee1448fd08d03f6daebdeadc6ea7c947f4f37e
NullNois3 <59552532+NullNois3@users.noreply.github.com>
2023-12-11 22:08:56 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,1 @@
-module "service_account_default_iam_grants" {
-  source  = "terraform-google-modules/org-policy/google"
-  version = "~> 3.0.2"
-
-  policy_for      = "folder"
-  constraint      = "constraints/compute.disableAllIpv6"
-  policy_type     = "boolean"
-  folder_id       = "235730749365"
-  enforce         = true
-}
 
