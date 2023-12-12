d2da823eb38ba62966ca618564ea2a78980790d8
NullNois3 <59552532+NullNois3@users.noreply.github.com>
2023-12-12 17:07:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,28 @@ module "service_account_default_iam_grants" {
   folder_id       = "235730749365"
   enforce         = true
 }
+
+# module "domain_restricted_sharing_constraint" {
+#   source           = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+#   version          = "~> 5.2.0"
+
+#   policy_root      = "organization"    # either of organization, folder or project
+#   policy_root_id   = ""       # either of org id, folder id or project id
+#   constraint       = "constraints/iam.allowedPolicyMemberDomains"    # constraint identifier without constriants/ prefix. Example "compute.requireOsLogin"
+#   policy_type      = "list"            # either of list or boolean
+
+#   rules = [
+#     # Rule 1
+#     {
+#       enforcement = false
+#       allow       = [""]
+#       deny        = []
+#       conditions  = [{
+#         description = "exempt via tag disable"
+#         expression  = "!resource.matchTag('/disable', 'domain-restricted-sharing')"
+#         location    = "sample-location.log"
+#         title       = "default"
+#       }]
+#     }
+#   ]
+# }
