925d020565230f32aa55aa409d23d5625eb5e52e
NullNois3 <59552532+NullNois3@users.noreply.github.com>
2023-12-14 18:39:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,3 +34,29 @@ module "service_account_default_iam_grants" {
 #     }
 #   ]
 # }
+
+
+module "resourceLocation" {
+  source           = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+  version          = "~> 5.2.0"
+
+  policy_root      = "organization"    # either of organization, folder or project
+  policy_root_id   = var.organization_id       # either of org id, folder id or project id
+  constraint       = "gcp.resourceLocations"    # constraint identifier without constriants/ prefix. Example "compute.requireOsLogin"
+  policy_type      = "list"            # either of list or boolean
+
+  rules = [
+    # Rule 1
+    {
+      enforcement = false
+      allow       = []
+      deny        = ["in:asia-east1-locations", "in:asia-east2-locations"]
+      conditions  = [{
+        description = "exempt via tag disable"
+        expression  = "!resource.matchTag('/disable', 'domain-restricted-sharing')"
+        location    = "sample-location.log"
+        title       = "default"
+      }]
+    }
+  ]
+}
